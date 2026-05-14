fc08c7db848013e6957827e2c9c5a4b8ad801d5e
dengyan <dengyan.hudou@bytedance.com>
2026-05-14 15:12:40 +0800

    adding shared_from_this test

--- questions/02_smart_pointer/smart_pointer.cpp
+++ questions/02_smart_pointer/smart_pointer.cpp
@@ -1,8 +1,11 @@
 #include <algorithm>
+#include <iostream>
 #include <mutex>
 #include <cstddef>
 #include <memory>
 #include <atomic>
+#include <ostream>
+#include <sstream>
 #include <vector>
 
 // the data infromation struct
@@ -10,7 +13,20 @@ struct MarketData {
     size_t symbol_id;
     double price;
     size_t timestamp;
+    std::string to_string() const {
+        std::ostringstream oss;
+        oss << "MarketData {"
+            <<"symbol_id = " << symbol_id
+            <<" price = " << price
+            <<" timestamp = "<< timestamp
+            <<"}";
+        return oss.str();
+    }
 };
+
+std::ostream& operator<<(std::ostream& os, const MarketData& data) {
+    return os << data.to_string();
+}
 
 class Subscriber : public std::enable_shared_from_this<Subscriber> {
     public:
@@ -31,6 +47,8 @@ class Publisher {
         std::mutex write_mtx_;
 
     public:
+        Publisher() : subscriber_list_(std::make_shared<SubList>()) {}
+
         // record subscriber
         void subscribe(std::shared_ptr<Subscriber> sub) {
             std::lock_guard<std::mutex> guard(write_mtx_);
@@ -75,3 +93,35 @@ class Publisher {
             }
         }
 };
+
+class SomeStrategy : public Subscriber {
+    private:
+        int id_;
+        Publisher& publisher_;
+    public:
+        SomeStrategy (int id, Publisher& publisher) : id_(id), publisher_(publisher) {}
+        ~SomeStrategy() = default;
+        void on_market_data(const MarketData& data) override {
+            std::cout << "subscriber "<< id_ 
+                      << " recive data " << data 
+                      << std::endl;
+            publisher_.unsubscribe(shared_from_this());
+        }
+        int get_id() const override {
+            return id_;
+        }
+};
+
+int main() {
+    Publisher publisher;
+
+    auto subscriber_1 = std::make_shared<SomeStrategy>(1, publisher);
+    auto subscriber_2 = std::make_shared<SomeStrategy>(2, publisher);
+
+    publisher.subscribe(subscriber_1);
+    publisher.subscribe(subscriber_2);
+    
+    MarketData data {1, 100.0,1001};
+    publisher.publish(data);
+    return 0;
+}
